1f0a57a1869e2d59fbc622cee6104f6a619d723a
Mosekim <kimmose1992@gmail.com>
2024-11-25 17:38:38 +0900

    인공지능을 위한 파이썬 > 3-4 모듈이란

---
+++ trainingProject/sparta/lecture/01_python_basic/lecture_3-4.py
@@ -0,0 +1,14 @@
+# math 모듈 import
+import math
+
+print(math.sqrt(16))
+
+# math 모듈 import => alias 설정
+import math as m
+
+print(m.sqrt(16))
+
+# math 모듈 import => 특정 함수만 지정
+from math import sqrt
+
+print(sqrt(16))
